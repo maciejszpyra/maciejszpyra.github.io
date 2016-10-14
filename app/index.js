cbf01b0543e4718dca4f29dbe8d416e750db44f8
Maciej Szpyra <maciej.szpyra@seargin.com>
2016-10-14 14:39:23 +0200

    Working slideshow. BUG: Third slide is visible on startup

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 (function () {
-    function init() {
-        var slides = document.getElementsByClassName('slide');
-        // Hide all slides apart from the active one
-        TweenLite.set(slides, {autoAlpha: 0});
+    var $activeSlide = $(".active"),
+        $slides = $(".slide"),
+        $hero = $(".hero");
 
+    function init() {
+        // TweenLite.set($slides.not($activeSlide), {autoAlpha: 0});
+        TweenLite.set($slides.not($activeSlide), {opacity: 0.5})
     }
 
     function nextSlide() {
@@ -14,7 +16,10 @@
     }
 
     function prevSlide() {
-        alert('Left was pressed');
+        var slideOut = document.getElementsByClassName('slide active')[0];
+        var slideIn = slideOut.previousElementSibling;
+        changeSlide(slideOut, slideIn);
+
     }
 
     function changeSlide(slideOut, slideIn) {
@@ -30,10 +35,10 @@
             // .to([slideOutH1,slideOutP], 0.3, {y: '-=15px', autoAlpha: 0, ease:Power3.easeInOut}, 0)
 
             // animate active slide up (out of the viewport)
-            .to(slideOut, 0.5, {y: '-100%', ease: Power3.easeInOut}, 0)
+            .to(slideOut, 1, {y: '-100%', ease: Power3.easeInOut}, 0)
 
             // animate new slide up (from out of the viewport)
-            .to(slideIn, 0.5, {y: '-=100%', ease: Power3.easeInOut}, 0);
+            .to(slideIn, 1, {y: '-=100%', ease: Power3.easeInOut}, 0);
 
         // animate H1 and P of the new slide up and fade them in
         // .fromTo([slideInH1,slideInP], 0.3, {y: '+=20px', autoAlpha: 0}, {autoAlpha: 1, y: 0, ease:Power1.easeInOut}, 0.3);

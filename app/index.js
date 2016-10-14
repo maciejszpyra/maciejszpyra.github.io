2010cf57c5171c2dafced7e59ae11bea976b38ba
Maciej Szpyra <maciej.szpyra@seargin.com>
2016-10-14 16:47:37 +0200

    Added option to slide up and down

--- app/index.js
+++ app/index.js
@@ -4,30 +4,51 @@
         $hero = $(".hero");
 
     function init() {
-        TweenLite.set($('.slide').not('.active'), {autoAlpha: 0});
+        var slides = document.getElementsByClassName('slide');
+        if(slides.length > 0 ){
+            var inactiveSlides =Array.prototype.slice.call(slides).filter(function (elem) {
+                return elem !== document.getElementsByClassName('slide active')[0];
+            });
+            TweenLite.set(inactiveSlides, {opacity: 0.5});
+            TweenLite.set(inactiveSlides, {autoAlpha: 0});
+        }
+
         // console.log();
-        // TweenLite.set($slides.not($activeSlide), {opacity: 0.5})
+        //
     }
 
     function nextSlide() {
         var slideOut = document.getElementsByClassName('slide active')[0];
         var slideIn = slideOut.nextElementSibling;
-        changeSlide(slideOut, slideIn);
+        changeSlide(slideOut, slideIn, 'up');
 
     }
 
     function prevSlide() {
         var slideOut = document.getElementsByClassName('slide active')[0];
         var slideIn = slideOut.previousElementSibling;
-        changeSlide(slideOut, slideIn);
+        changeSlide(slideOut, slideIn, 'down');
 
     }
 
-    function changeSlide(slideOut, slideIn) {
+    function changeSlide(slideOut, slideIn, direction) {
+        var slideInY = '';
+        var slideOutY = '';
+        var slideInDiff = '';
+        if(direction ==='up'){
+             slideInY = '100%';
+            slideOutY = '-100%';
+            slideInDiff = '-=100%'
+        } else {
+             slideInY = '-100%';
+            slideOutY = '100%';
+            slideInDiff = '+=100%';
+        }
         var tl = new TimelineLite();
         tl
         // move the new slide (the one about to enter viewport) out of the viewport and add class active
-            .set(slideIn, {y: '100%', autoAlpha: 1, className: '+=active'})
+
+            .set(slideIn, {y: slideInY, autoAlpha: 1, className: '+=active'})
 
             // remove class active from the currently active slide (slideOut)
             .set(slideOut, {className: '-=active'})
@@ -36,10 +57,10 @@
             // .to([slideOutH1,slideOutP], 0.3, {y: '-=15px', autoAlpha: 0, ease:Power3.easeInOut}, 0)
 
             // animate active slide up (out of the viewport)
-            .to(slideOut, 1, {y: '-100%', ease: Power3.easeInOut}, 0)
+            .to(slideOut, 1, {y: slideOutY, ease: Power3.easeInOut}, 0)
 
             // animate new slide up (from out of the viewport)
-            .to(slideIn, 1, {y: '-=100%', ease: Power3.easeInOut}, 0);
+            .to(slideIn, 1, {y: slideInDiff, ease: Power3.easeInOut}, 0);
 
         // animate H1 and P of the new slide up and fade them in
         // .fromTo([slideInH1,slideInP], 0.3, {y: '+=20px', autoAlpha: 0}, {autoAlpha: 1, y: 0, ease:Power1.easeInOut}, 0.3);
